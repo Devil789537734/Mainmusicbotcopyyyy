5e18d5aa0d08a65c586f64c872061bf430910fa5
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2023-10-17 20:58:12 +0530

    Update __main__.py

--- AarohiX/__main__.py
+++ AarohiX/__main__.py
@@ -52,12 +52,12 @@ async def init():
     await userbot.start()
     await Aarohi.start()
     try:
-        await Aarohi.stream_decall("https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4")
+        await Aarohi.stream_decall("https://telegra.ph/file/440427330b7871341f2cf.mp4")
     except:
         pass
     try:
         await Aarohi.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://telegra.ph/file/440427330b7871341f2cf.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AarohiX").error(
